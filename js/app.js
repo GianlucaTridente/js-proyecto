9b3e117445efa81085759ab3a8211f7fff80d046
Gianluca Tridente <tridentegianluca03@gmail.com>
2022-08-26 19:17:41 -0300

    simulador interactivo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let juan = 0;
+/* let juan = 0;
 let zoe  = 0 ;
 let agustin  = 0;
 
@@ -30,6 +30,77 @@ while (finalizar != true) {
 
 console.log("Juan tuvo " + juan + " voto");
 console.log("Zoe tuvo " + zoe + " voto");
-console.log("Agustin tuvo " + agustin + " voto");
+console.log("Agustin tuvo " + agustin + " voto"); */
 
 
+/* simulador interactivo */
+/* coutas sin interes */
+
+alert ("Cuotas sin interes");
+
+let precioDelProducto = prompt("Ingresa el precio del producto");
+
+function coutas (num1, num2){
+    let numero = num1 / num2;
+
+    if (isNaN (num1) || isNaN(num2)){
+        alert ("Los resultados deben ser numero");
+    }
+    else{
+        alert ("Serian " + cantidadDeCuotas + " coutas de $" + numero );
+    }
+}
+
+let cantidadDeCuotas = prompt("elegi si lo queres pagar en: \n 3 Cuotas \n 6 Cuotas \n 12 Cuotas");
+
+
+switch (cantidadDeCuotas) {
+    case "3":
+        coutas (precioDelProducto , 3);
+        break;
+    case "6":
+        coutas (precioDelProducto , 6);
+        break;
+    case "12":
+        coutas (precioDelProducto , 12);
+        break;
+    default:
+        alert ("No disponemos de otras opciones de cuotas")
+        break;
+}
+
+
+alert ("Cuotas con interes");
+
+function interes (numero1, numero2, numero3){
+    let valorPorcentaje = ((numero1 * numero3) / 100) / numero2;
+    let resultado = (numero1 / numero2) + valorPorcentaje;
+    
+    
+
+    if (isNaN (numero1) || isNaN(numero2) || isNaN (numero3)){
+        alert ("Los resultados deben ser numero");
+    }
+    else{
+        alert ("Serian " + cantidadDeCuotasInteres + " coutas de $" + resultado );
+    }
+}
+
+let precioDelProductoInteres = prompt("Ingresa el precio del producto");
+let cantidadDeCuotasInteres = prompt("elegi si lo queres pagar en: \n 3 Cuotas \n 6 Cuotas \n 12 Cuotas");
+let porcentaje = prompt ("Ingrese el porcentaje de interes");
+
+switch (cantidadDeCuotasInteres) {
+    case "3":
+        interes (precioDelProductoInteres , 3, porcentaje);
+        break;
+    case "6":
+        interes (precioDelProductoInteres , 6, porcentaje);
+        break;
+    case "12":
+        interes (precioDelProductoInteres , 12, porcentaje);
+        break;
+    default:
+        alert ("No disponemos de otras opciones de cuotas")
+        break;
+}
